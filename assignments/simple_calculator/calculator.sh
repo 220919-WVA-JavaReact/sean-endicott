96745069b140fca9f220e7d08e03d4c8d01e1947
seane13 <seanendicott76@gmail.com>
2022-09-22 13:58:50 -0400

    duplicate update calculator on main branch

--- assignments/simple_calculator/calculator.sh
+++ assignments/simple_calculator/calculator.sh
@@ -1,27 +1,45 @@
+
 #!/bin/bash
 
 # save user input values into variables
-read -p "enter first number: " a; 
+read -p "enter first number: " a;
 read -p "enter second number: " b;
+
+if [[ $a =~ ^[0-9]+$ && $b =~ ^[0-9]+$ ]]; then
+        echo "you entered $a and $b"
+else
+        echo "Please choose a valid number"
+        exec bash "$0"
+fi
 
 
 # using ps3 variable and select operator
 # used bc-> basic calculator for decimals
+
 ps3="Please select an operator to perform on your numbers"
 select operator in add subtract multiply divide;
 do
-	echo "you chose to $operator your numbers"
-	if [[ $operator == "add" ]]; then
-	echo $(( $a + $b ))
-	elif [[ $operator == "subtract" ]]; then
-	echo $(( a - b ))
-	elif [[ $operator == "multiply" ]]; then
-	echo $(( a * b ))
-	elif [[ $operator == "divide" ]]; then
-	echo  "scale=2; $a / $b"  | bc 
-	fi
+
+
+        if [[ $operator == "add" ]]; then
+                echo "$a $operator $b = " $(( $a + $b ))
+        elif [[ $operator == "subtract" ]]; then
+                echo "$a $operator $b = " $(( a - b ))
+        elif [[ $operator == "multiply" ]]; then
+                echo "$a $operator $b = " $(( a * b ))
+        elif [[ $operator == "divide" ]]; then
+                echo "$a $operator $b = "
+                echo "scale=2; $a / $b"  | bc
+        else
+                echo "Please chose a valid number"
+                exec bash "$0"
+        fi
 break
 done
+
+
+
+
 
 
 
