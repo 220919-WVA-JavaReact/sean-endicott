45e013cec1cf8a0adc4aa5fd5e24e4174d6aa9dd
seane13 <seanendicott76@gmail.com>
2022-09-25 15:54:30 -0400

    updated assignments

---
+++ assignments/week1/UNIX_class_assignment/Q3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+read -p "enter number: " n;
+
+for num in {n..0}
+do
+  echo num * n
+done
+
+#for ((i=0: i<n; i-- ))
+#do
+#  a=$((n * (n-1)
---
+++ assignments/week1/redirect/redir_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+read -r -p "Enter first name: " fname;
+read -r -p "Enter last name: " lname;
+read -r -p "Enter email: " email;
+read -r -p "Enter user name: " uname;
+read -r -p "Enter password: " pw;
+echo "$fname,$lname,$email,$uname,$pw" >> data.csv
+
+
+
+
+  
+
+ 
